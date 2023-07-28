d4d9710e3386d0cf62e403ca832ce6957ca5c4f3
Dos-Anjos-pedro <edmilsonpedropedro3@gmail.com>
2023-07-28 20:33:00 +0100

    modified src/products/views.py and create src/products/templates/featured-detail.html and modified src/products/templates/detail.html

--- products/views.py
+++ products/views.py
@@ -5,6 +5,20 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    #def get_queryset(self, *args, **kwargs):
+        #request = self.request
+        #return Product.objects.featured()
+
 #Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
@@ -23,18 +37,17 @@ def product_list_view(request):
     context = {
         'object_list': queryset
     }
-    return render(request, "products/list.html", context)
+    return render(request,"products/list.html", context)
 
 #Class Based View
 class ProductDetailView(DetailView):
-    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
-    
+
     def get_object(self, *args, **kwargs):
         pk = self.kwargs.get('pk')
         instance = Product.objects.get_by_id(pk)
