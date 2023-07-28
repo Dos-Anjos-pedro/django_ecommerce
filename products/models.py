50b6485347362129b5fe6aef6cce9d2f22764fc4
Dos-Anjos-pedro <edmilsonpedropedro3@gmail.com>
2023-07-28 11:35:49 +0100

    create custom productManager class in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id = id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
