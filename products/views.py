1d11b098cb52f65bdf9884d980de96583c7f2e19
Dos-Anjos-pedro <edmilsonpedropedro3@gmail.com>
2023-07-28 11:18:33 +0100

    optimize product_detail_view in src/products/views.py

--- products/views.py
+++ products/views.py
@@ -40,10 +40,10 @@ def product_detail_view(request,pk = None,*args, **kwargs):
    #instance = Product.objects.get(pk = pk)
    # instance = get_object_or_404(Product, pk = pk)
    # queryset = Product.objects.all()
-    try:
-        instance = Product.objects.get(id = pk)
-    except Product.DoesNotExist:
-        print("Nenhum produto encontrado aqui!")
+    qs = Product.objects.filter(id = pk)
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
         raise Http404("Esse produto não existe!")
     context = {
         'object': instance
