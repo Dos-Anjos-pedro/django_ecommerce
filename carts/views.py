8187cdeabd43d62b76f5dea2e4b32b33095b3d27
Dos-Anjos-pedro <edmilsonpedropedro3@gmail.com>
2023-09-27 03:53:38 +0100

    i do it

--- carts/views.py
+++ carts/views.py
@@ -9,11 +9,15 @@ def cart_home(request):
 
 def cart_update(request):
     product_id = 5
-    # Pega o produto com id 5
     product_obj = Product.objects.get(id=product_id)
-    # Cria ou pega a instância já existente do carrinho
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    # E o produto se adiciona a instância do campo M2M 
-    cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-    #cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
-    return redirect(product_obj.get_absolute_url())
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
+    else:
+        # E o produto se adiciona a instância do campo M2M 
+        cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+
+   
+    
+    #return redirect(product_obj.get_absolute_url())
+    return redirect("cart:home")
